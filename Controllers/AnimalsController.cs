547fc7a3ce365631c6978d65d58a9e84e0b607e1
Laura Kehres <kehres.39@osu.edu>
2018-04-16 20:20:40 -0400

    created CRUD for animals

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -13,39 +13,109 @@ namespace Sheep.SiteV2.Api.Controllers
     public class AnimalsController : Controller
     {
     
-       private readonly SheepSiteV2Context db;
+      private readonly SheepSiteV2Context db;
+      public AnimalsController(SheepSiteV2Context db)
+        {
+            this.db = db;
 
-       public AnimalsController(SheepSiteV2Context db)
+            if(this.db.Animals.Count() == 0)
+            {
+                this.db.Animals.Add(new Animal {
+                    Id = 1,
+                    sheepDOB = default(DateTime),
+                    sheepGender = "F"
+                });
+                
+                this.db.Animals.Add(new Animal
+                {
+                    Id = 2,
+                    sheepDOB = default(DateTime),
+                    sheepGender = "M"
+                });
+
+                this.db.SaveChanges();
+            }
+        }
+        [Route("api/[controller]/[action]")]
+       [HttpGet]
+       public IActionResult Get()
        {
-           this.db = db;
+            return Ok(db.Animals);
        }
        [HttpGet]
        public IActionResult Index()
        {
-           return View(db.Animals);
+         return View(db.Animals);
        }
-       // private IEnumerable<Animal> GetAnimals()
-        //{
-          //  var animal = new List<Animal>();
 
-            //animal.Add(new Animal(){
-              //  Id = 1,
-                //sheepDOB = default(DateTime),
-              //  sheepGender = "F"
-            //});
+        [Route("api/[controller]/[action]/{id?}")]
+        [HttpGet("{id}", Name="GetAnimal")]
+        public IActionResult Get(int id)
+        {
+            var animal = db.Animals.Find(id);
+            
+            if(animal == null)
+            {
+                return NotFound();
+            }
+            return Ok(animal);
+        }
 
-            //animal.Add(new Animal(){
-              //  Id = 2,
-                //sheepDOB = default(DateTime),
-                //sheepGender = "M"
-            //});
+        [Route("api/[controller]")]
+        [HttpPost]
+        public IActionResult Post([FromBody]Animal animal)
+        {
+            if(animal == null)
+            {
+                return BadRequest();
+            }
 
-            //return animal;
-        //}
-        //public IActionResult Index()
-        //{
-            //var animal = GetAll();
-          //  return View(animal);
-        //}
+            this.db.Animals.Add(animal);
+            this.db.SaveChanges();
+
+            return CreatedAtRoute("GetAnimal", new { id = animal.Id}, animal);
+        }
+
+        [Route("api/[controller]/[action]/{id?}")]
+        [HttpPut("{id}")]
+
+        public IActionResult Put(int id, [FromBody]Animal newAnimal)
+        {
+            if (newAnimal == null || newAnimal.Id !=id)
+            {
+                return BadRequest();
+            }
+            var currentAnimal = this.db.Animals.FirstOrDefault(x => x.Id == id);
+
+            if (currentAnimal == null)
+            {
+                return NotFound();
+            }
+
+            currentAnimal.sheepDOB = newAnimal.sheepDOB;
+            currentAnimal.sheepGender = newAnimal.sheepGender;
+
+            this.db.Animals.Update(currentAnimal);
+            this.db.SaveChanges();
+
+            return NoContent();
+        }
+
+        [Route("api/[controller]/[action]/{id?}")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var animal = this.db.Animals.FirstOrDefault(x => x.Id == id);
+
+            if (animal == null)
+            {
+                return NotFound();
+            }
+            
+            this.db.Animals.Remove(animal);
+            this.db.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
